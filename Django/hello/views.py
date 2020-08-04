7e57ffbb5f7ad5c13e8392694629d61d4cbbf2f9
Bhunesh Patel <Bhunesh>
2020-08-04 20:37:16 +1000

    Create views.py

---
+++ Django/hello/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+def Bhunesh(request):
+    return HttpResponse("Hello, Bhunesh")
+
+def David(request):
+    return HttpResponse("Hello, David")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
